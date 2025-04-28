0a29b2b45f5f6d2bee0af7d59d763ac0eb6636db
soliumcoin1453 <soliumcoin@gmail.com>
2025-04-28 15:28:48 +0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  resolve: {
+    fallback: {
+      stream: require.resolve('stream-browserify'),
+      assert: require.resolve('assert/'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      url: require.resolve('url/')
+    }
+  }
+};
